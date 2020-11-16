484dd8a01b74d4daa794f9fea78f5d8ca1b0a64e
Vin-commit <74498336+Vin-commit@users.noreply.github.com>
2020-11-16 20:39:43 +0100

    Add files via upload

---
+++ phpinfo.php
@@ -0,0 +1,15 @@
+﻿<?php include("./exit.php"); ?>
+
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="icon" href="/Images/favicon.ico" />
+    <link href="/style.css" rel="stylesheet" media="all" type="text/css">
+    <title>Quelques outils de base et techniques pour tout contenu de site web</title>
+  </head>
+  <body>
+    <? phpinfo() ?>
+    <p class=src><a href="/FichiersTexte/phpinfo.txt">source php de la page</a></p>
+  </body>
+</html>
