61dd8a93b2cd8842fd2e80e309cabbc180cac4f8
Vin-commit <74498336+Vin-commit@users.noreply.github.com>
2020-11-24 18:57:33 +0100

    Update phpinfo.php

--- phpinfo.php
+++ phpinfo.php
@@ -1,15 +1,13 @@
-﻿<?php include("./exit.php"); ?>
-
-<!doctype html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <link rel="icon" href="/Images/favicon.ico" />
-    <link href="/style.css" rel="stylesheet" media="all" type="text/css">
-    <title>Quelques outils de base et techniques pour tout contenu de site web</title>
-  </head>
-  <body>
-    <? phpinfo() ?>
-    <p class=src><a href="/FichiersTexte/phpinfo.txt">source php de la page</a></p>
-  </body>
-</html>
+﻿      1 <?php
+      2 // Mettre en commentaires la ligne 5 pour permettre d'exécuter la suite du fichier appelant.
+      3 // Ce fichier est inclus dans tous les fichiers php du site.
+      4 
+      5 //exit ("Access forbidden.");
+      6 
+      7 
+      8 // Si ce fichier est appelé directement :
+      9 if($PHP_SELF == "/exit.php") {
+     10    echo "<!doctype html>\n<html>\n<head>\n<meta charset=\"utf-8\">\n<link href=\"style.css\" rel=\"stylesheet\" media=\"all\" type=\"text/css\">\n<link rel=\"icon\" href=\"Images/favicon.ico\" />\n</head>\n<body>\nPermet d'exécuter la suite ou non du fichier appelant.";
+     11    echo "<p class=src><a href=\"FichiersTexte/exit.txt\">source php de la page</a></p>\n</body>\n</html>";
+     12 }
+     13 ?>
